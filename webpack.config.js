38001de5e270403ad550686818659556f7ddc4fa
Tushar <singhtushar348@gmail.com>
2022-10-02 01:06:23 +0530

    Configure webpack with plugins and loaders

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/src/index.html',
+  filename: 'index.html',
+  inject: 'head'
+})
+module.exports = {
+  entry: __dirname + '/src/index.js',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/build'
+  },
+  plugin: [HTMLWebpackPluginConfig] 
+};
