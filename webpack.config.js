42ee2eaf50a31d71613fc7aa3905f88dc6f66a43
Tushar <singhtushar348@gmail.com>
2022-10-02 02:00:11 +0530

    Update webpack with rules and use

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,21 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/src/index.html',
   filename: 'index.html',
   inject: 'head'
 })
+
 module.exports = {
   entry: __dirname + '/src/index.js',
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        use: {
+          loader: 'babel-loader'
+        },
       }
     ]
   },
@@ -19,5 +23,5 @@ module.exports = {
     filename: 'bundle.js',
     path: __dirname + '/build'
   },
-  plugin: [HTMLWebpackPluginConfig] 
+  plugins: [HTMLWebpackPluginConfig] 
 };
