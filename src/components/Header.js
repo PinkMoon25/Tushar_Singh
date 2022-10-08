d3e37503b71fbe112fa2c2f0d538c63d3e5c8bfc
Tushar <singhtushar348@gmail.com>
2022-10-08 22:49:20 +0530

    Add header section

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = ()=>{
+  return (
+    <header>
+      <h1>Tushar</h1>
+      <nav>
+        <a>Home</a>
+        <a>Portfolio</a>
+        <a>Contact</a>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
