f89e22ca3db93b5a558bdfd28443638a9a382e58
Tushar <singhtushar348@gmail.com>
2022-10-09 22:20:39 +0530

    Add portfolio section

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./Hero";
 import Header from "./Header";
+import Portfolio from "./Portfolio";
 
 const App = () => {
   return (
     <>
       <Header/>
       <Hero/>
+      <Portfolio />
     </>
   );
 };
