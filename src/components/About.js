70e89e07ffa8f2811e3aaff65b7dd476661d06ee
Tushar <singhtushar348@gmail.com>
2022-10-16 23:22:38 +0530

    Create tech stack card

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,21 @@ const About = () => {
   return (
     <section className="about-section">
       <h1>About Me</h1>
-      <p>Born and raised in India. I am a passionate software developer and an avid learner.
-        I like all things Programming and Problem-solving.
-        I contribute and work as a Code Reviewer with Microverse.
-        I like Music&#127926;, Walk in a park	
-&#128694; and playing guitar for my leisure &#128516;
-      </p>
+        <p>Born and raised in India. I am a passionate software developer and an avid learner.<br/>
+          I like all things Programming and Problem-solving.<br/>
+          I contribute and work as a Code Reviewer with Microverse.<br/>
+          I like Music &#127926; &nbsp;Walk in a park &#128694; and playing guitar at my leisure &#128516;
+        </p>
+      <div className="tech-stack">
+        <h2 style={{textDecoration: 'underline'}}>Stack</h2>
+        <ul style={{fontWeight: 700}}>
+          <li>JavaScript </li>
+          <li>React </li>
+          <li>Redux </li>
+          <li>Ruby </li>
+          <li>Rails </li>
+        </ul>
+      </div>
       <a href={resume} target='_blank'><button>Résumé</button></a>
     </section>
   )
