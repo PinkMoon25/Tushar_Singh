4db3ed035df603e02e48f18a78e5420f04a4b8ad
Tushar <singhtushar348@gmail.com>
2023-01-03 00:18:09 +0530

    update image formats and size

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaGithub, FaLinkedin, FaTwitter, FaMediumM, FaAngellist } from "react-icons/fa";
-import pic from "../img/portfolio-profile.jpg";
+import pic from "../img/portfolio-profile.webp";
 import background from '../img/soundwave.svg';
 
 const Hero = () => {
